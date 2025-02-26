6ad52ead27a750393a307868b66ad293f119d444
Ahmet7851 <119795229+Ahmet7851@users.noreply.github.com>
2025-02-26 15:13:55 +0300

    Proje Eklenmesi
    Githuba Ekleme

--- TaskManager/Models/Task.cs
+++ TaskManager/Models/Task.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TaskManager.Models;
-
+// Deneme Satırı
 public class UserTask
 {
     [Key]
